b3e29266d29a021783c2679e37d28450b0590bb3
DhruvCS <74084741+DhruvCS@users.noreply.github.com>
2020-11-22 15:44:24 -0600

    Update SvsS.py

--- SvsS.py
+++ SvsS.py
@@ -5,4 +5,4 @@ import time
 x=1
 while x == 1:
     time.sleep(2700)
-    webbrowser.open('my url here', new=3)
+    webbrowser.open('https://dhruvcs.github.io/SittingvsStanding/prototype.html', new=3)
